af3e56dc94d8469ed2a1210154dab0d665af0f37
Nathan Fair <nathan.fair@gmail.com>
2015-12-14 16:59:43 -0800

    First commit, nothing much but data model set up

---
+++ FlashCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlashCard
+//
+//  Created by Nathan Fair on 12/11/15.
+//  Copyright © 2015 Nathan Fair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlashCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlashCard
+//
+//  Created by Nathan Fair on 12/11/15.
+//  Copyright © 2015 Nathan Fair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
